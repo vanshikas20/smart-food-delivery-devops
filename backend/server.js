cc64daaa6cf0ea2e4fc946025892915b4b56e577
vanshikas20 <vanshikas20105@gmail.com>
2026-04-14 14:36:10 +0530

    Updated package.json with start script

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+// Home route
+app.get('/', (req, res) => {
+    res.send('Smart Food Delivery Backend Running!');
+});
+
+// Menu API
+app.get('/menu', (req, res) => {
+    res.json([
+        { id: 1, name: "Pizza", price: 200 },
+        { id: 2, name: "Burger", price: 100 }
+    ]);
+});
+
+// Order API
+app.post('/order', (req, res) => {
+    res.json({
+        message: "Order placed successfully",
+        order: req.body
+    });
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
